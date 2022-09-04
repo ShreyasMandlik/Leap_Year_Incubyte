82a258de0501792aa06401362a40bfe64dfe4707
Shreyas <mandlikshreyas01@gmail.com>
2022-09-04 20:18:36 +0530

    Added 1 test case for Year divisible by 400

--- LeapYear.py
+++ LeapYear.py
@@ -4,5 +4,10 @@ def LeapYear(Year):
         str="Year cannot be negative"
         return str
     
-    
+    if Year % 400 ==0:
+        return True
+
+
+
+
 
--- test_leapYear.py
+++ test_leapYear.py
@@ -1,14 +1,20 @@
 
 import unittest
+from unittest import result
 import LeapYear
 
 
 class TestLeapYear(unittest.TestCase):
     
-    def test_LeapYear(self):
+    def test_leapYear(self):
+
         result=LeapYear.LeapYear(-2000)
-        self.assertEqual(result,"Year cannot be negative")  # If the year is negative it cannot be taken as Input 
+        stri="Year cannot be negative"
+        self.assertEqual(result,stri) 
+  
+        result1=LeapYear.LeapYear(2000)
+        self.assertEqual(result1,True)             # If the year is divisible by 400 then it is leap year
 
 
-    if __name__=="__main__":
-        unittest.main()
+if __name__=='__main__':
+    unittest.main()
